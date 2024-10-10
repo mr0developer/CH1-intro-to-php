5a5afa0fb1fbcf43700b57c4e450d6ed91e882e3
Asterdev! <108690167+mr0developer@users.noreply.github.com>
2024-10-10 12:26:28 +0300

    Create info.php

---
+++ info.php
@@ -0,0 +1,12 @@
+<?php
+    // Declaring variables
+    $rollNumber = "CIT-227-054/2022";
+    $courseName = "Web Development";
+    $technologyName = "PHP";
+
+    // Displaying the information on the browser
+    echo "<h1>Student Information</h1>";
+    echo "<p><strong>Roll Number:</strong> " . $rollNumber . "</p>";
+    echo "<p><strong>Course Name:</strong> " . $courseName . "</p>";
+    echo "<p><strong>Technology Name:</strong> " . $technologyName . "</p>";
+?>
